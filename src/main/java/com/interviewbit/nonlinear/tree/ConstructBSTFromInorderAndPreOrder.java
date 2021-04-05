ddd30b0e0e8202d6e4fe46a7bd2a923957a13a15
tavisca-tjajodia <tavisca-tjajodia@github.com>
2021-04-05 14:14:43 +0545

    Updated the code of ConstructBST from inOrder and preOrder as it was failing for larger test cases

--- src/main/java/com/interviewbit/nonlinear/tree/ConstructBSTFromInorderAndPreOrder.java
+++ src/main/java/com/interviewbit/nonlinear/tree/ConstructBSTFromInorderAndPreOrder.java
@@ -11,49 +11,40 @@ public class ConstructBSTFromInorderAndPreOrder {
     public static void main(String[] args) {
 
         ConstructBSTFromInorderAndPreOrder obj = new ConstructBSTFromInorderAndPreOrder();
-        int[] inOrder = ArrayUtils.asArrays(5, 6, 7, 8, 9, 10, 11, 12, 13, 19, 25);
-        int[] preOrder = ArrayUtils.asArrays(10, 8, 6, 5, 7, 9, 12, 11, 19, 13, 25);
+//        int[] preOrder = ArrayUtils.asArrays(11, 14, 21, 20, 18, 28, 1, 10, 30, 26, 25, 8, 2, 4, 19, 27, 12, 6, 9, 23, 29, 17, 15, 31, 5, 16, 3, 24, 22, 13, 7);
+//        int[] inOrder = ArrayUtils.asArrays(28, 1, 18, 10, 20, 30, 26, 25, 8, 2, 21, 4, 19, 14, 27, 11, 9, 23, 6, 17, 15, 29, 31, 12, 3, 16, 5, 24, 7, 13, 22);
 
+        int[] preOrder = ArrayUtils.asArrays( 1, 2, 3 );
+        int[] inOrder = ArrayUtils.asArrays(2, 1, 3 );
         TreeNode root = obj.constructBST(inOrder, preOrder, 0, inOrder.length, 0, preOrder.length);
         ArrayList<Integer> inOrderList = new ArrayList<>();
         InorderTraversal.traverse(root,inOrderList);
-        System.out.println(inOrderList);
-
         ArrayList<Integer> preOrderList = new ArrayList<>();
         PreOrderTraversal.traverse(root,preOrderList);
-        System.out.println(preOrderList);
-    }
 
+        System.out.println("Pre Order Traversal"+preOrderList);
+        System.out.println("Inorder Traversal "+inOrderList);
+    }
 
     public TreeNode constructBST(int[] inOrder, int[] preOrder, int iStart, int iEnd, int pStart, int pEnd){
 
-        TreeNode node = new TreeNode(preOrder[pStart]);
-        if((iEnd - iStart) <= 1 || (pEnd - pStart) <= 1)
-            return node;
+        if( iStart >= iEnd || pStart >= pEnd) return null;
 
-        int leftSubTreeIEnd = -1;
+        int rootValue = preOrder[pStart];
+        TreeNode root = new TreeNode(rootValue);
+        int leftTreeEndIndex = -1;
         for(int i = iStart; i < iEnd; i++){
-            if( inOrder[i] == preOrder[pStart]){
-                leftSubTreeIEnd = i;
+            if(rootValue == inOrder[i]){
+                leftTreeEndIndex = i;
                 break;
             }
         }
 
-//        int leftSubTreePEnd = -1;
-//        for(int i = pStart; i < pEnd; i++) {
-//            if (preOrder[i] == inOrder[leftSubTreeIEnd - 1]) {
-//                leftSubTreePEnd = i + 1;
-//                break;
-//            }
-//        }
-//      This Above Iteration Can Be Removed calculating the Number of Items in the Left and Right Sub Arrays as follows.
-        int numElements = leftSubTreeIEnd - iStart;
+        int numberOfElements = leftTreeEndIndex - iStart;
+        root.left = constructBST(inOrder,preOrder,iStart,leftTreeEndIndex,pStart + 1,pStart + 1 + numberOfElements);
+        root.right = constructBST(inOrder,preOrder,leftTreeEndIndex + 1, iEnd,pStart+numberOfElements + 1,pEnd);
 
-        node.left = constructBST(inOrder,preOrder,iStart,leftSubTreeIEnd,pStart + 1,pStart + numElements);
-        node.right = constructBST(inOrder,preOrder,leftSubTreeIEnd + 1,iEnd,pStart + numElements + 1,pEnd);
-
-        return node;
-
+        return root;
     }
 
 
